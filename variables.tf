5cf05614dd08087aacadb8cac3c37eec25bd0069
Anu6is <curtis.mayers@gmail.com>
2022-06-25 07:00:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,8 +3,6 @@
 
 variable "tenancy_ocid" {}
 variable "user_ocid" {}
-variable "fingerprint" {}
-variable "private_key_path" {}
 variable "region" {}
 variable "compartment_ocid" {}
 variable "availablity_domain_name" {}
