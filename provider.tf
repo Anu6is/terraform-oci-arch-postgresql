82e7c386d405b2976272794a7251356af32a5801
Anu6is <curtis.mayers@gmail.com>
2022-06-25 07:19:02 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -14,8 +14,6 @@ terraform {
 provider "oci" {
   tenancy_ocid     = var.tenancy_ocid
   user_ocid        = var.user_ocid
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
   region           = var.region
 }
 
@@ -23,8 +21,6 @@ provider "oci" {
   alias                = "homeregion"
   tenancy_ocid         = var.tenancy_ocid
   user_ocid            = var.user_ocid
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
   region               = data.oci_identity_region_subscriptions.home_region_subscriptions.region_subscriptions[0].region_name
   disable_auto_retries = "true"
 }
